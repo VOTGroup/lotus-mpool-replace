fa763cb72aabb53e3c668395fd4b8aaabae2b51d
VOTGroup <116676147+VOTGroup@users.noreply.github.com>
2023-05-25 09:28:12 -0600

    Update mpool_replace.py
    Updates to mistypes
    Added average age in epochs statistic
    Updated chain sync to account for epoch ticks during wait
    Moved stats output into it's own function

--- mpool_replace.py
+++ mpool_replace.py
@@ -43,13 +43,13 @@ def parse_args() -> argparse.Namespace:
     )
     parser.add_argument(
         "--working-fee-increase",
-        help="This is the percentage of the minimum fee you would like to add to the next round fee each round (linear mode) or the amount of the previous round fee you want to add to the next round fee each round (exponential mode). (Default: 25%)",
+        help="This is the percentage of the minimum fee you would like to add to the next round fee each round linear mode or the amount of the previous round fee you want to add to the next round fee each round (exponential mode). (Default: 25%%)",
         type=float,
         default=os.environ.get("WORKING_FEE_INCREASE", 25),
         required=False,
     )
     parser.add_argument(
-        "-working-fee-mode",
+        "--working-fee-mode",
         help="This is the fee replacement mode see reference on --working-fee-increase for more detail. (Default: linear)",
         type=str,
         default=os.environ.get("WORKING_FEE_MODE", "linear"),
@@ -92,12 +92,13 @@ data = {
         "total_worked": 0,
         "total_replaced": 0,
         "max_fee": 0,
-        "avg_fee": 0
+        "avg_fee": 0,
+        "avg_age": 0
     }
 }
 dataFile = "mpool_replace.json"
 
-# Store working threads for termination
+# Store working threads for termination on exit
 worker_threads = []
 
 # Configure the logger for both stdout and file output
@@ -150,8 +151,8 @@ def processing_pipeline(options: dict):
                     statistics["total_pending"] += 1
 
             # Process pending messages and move them into working messages if they've aged out
-            for cid in list(pending_messages):
-                
+            for cid in list(pending_messages):    
+
                 # If pending message is no longer in mpool remove it
                 if cid not in mpool_cids:
                     log.info(f"Pending message: {cid} was processed on chain with no required fee change so removing from tracker.")
@@ -159,14 +160,16 @@ def processing_pipeline(options: dict):
 
                 # Else if it's not in working messages yet see if it's aged out and add it to working messages
                 elif cid not in working_messages and statistics["current_epoch"] > pending_messages[cid] + options.pending_wait_epochs: 
+                    
                     # Add to working messages
                     working_messages[cid] = {
                         "start_epoch": pending_messages[cid],
                         "round_epoch": pending_messages[cid],
                         "round_fee": options.min_fee,
                         "last_round_fee": options.min_fee,
-                        "current_age": 0
+                        "total_age": 0
                     }
+                    
                     # Remove from pending messages
                     del pending_messages[cid] #
                     log.info(f"Pending message: {cid} has expired initial wait epochs of {options.pending_wait_epochs} and moved into working tracker.")
@@ -178,10 +181,14 @@ def processing_pipeline(options: dict):
                 message = working_messages[cid]
                 
                 # If working message is no longer in mpool remove it and record basic runtime stats
-                if cid not in mpool_cids:
+                if cid not in mpool_cids:                  
+                    # Statistics
                     last_round_fee = message["last_round_fee"]
+                    total_age = message["total_age"]
                     statistics["max_fee"] = last_round_fee if last_round_fee > statistics["max_fee"] else statistics["max_fee"]
                     statistics["avg_fee"] = last_round_fee if statistics["total_replaced"] == 1 else (statistics["avg_fee"] + last_round_fee) / 2
+                    statistics["avg_age"] = total_age if statistics["total_replaced"] == 1 else (statistics["avg_age"] + total_age) / 2
+                    # Remove message
                     del working_messages[cid]
                     log.info(f"Worker message: {cid} was removed from tracking as it was processed on chain with a round fee of {last_round_fee}.")
 
@@ -195,9 +202,9 @@ def processing_pipeline(options: dict):
                         process = subprocess.Popen(["lotus", "mpool", "replace", "--auto", "--fee-limit", str(fee_to_execute), cid], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                         stdout, stderr = process.communicate()
                         
-                        if stderr:
-                            
+                        if stderr:                
                             error = stderr.decode('utf-8')
+                            # Handle to low a gas premium error by essentially skipping this round but still upping fee
                             if "too low GasPremium" in error:
                                 new_fee = get_next_round_fee(options, message["round_fee"])
                                 log.warning(f"Tried replacing message, but fee difference was to low. Upping fee in place to {new_fee} for next loop without replacing")
@@ -210,15 +217,17 @@ def processing_pipeline(options: dict):
                             
                             # Add new message to be tracked and increase the fee for the next round
                             next_round_fee = get_next_round_fee(options, message["round_fee"])
+                            total_age = statistics["current_epoch"] - message["start_epoch"]
                             working_messages[new_cid] = {
                                 "start_epoch": message["start_epoch"],
                                 "round_epoch": statistics["current_epoch"], 
                                 "round_fee": next_round_fee,
-                                "last_round_fee": fee_to_execute
+                                "last_round_fee": fee_to_execute,
+                                "total_age": total_age
                             }
                             del working_messages[cid]
                             # Log out some information
-                            total_age = statistics["current_epoch"] - message["start_epoch"]
+
                             log.info(f"Worker message: {cid} has been replaced with new worker message: {new_cid}..")
                             log.info(f"Stats for new msg: {new_cid} [Total age: {total_age} epochs] [Next round fee: {next_round_fee}]")
                             statistics["total_replaced"] += 1
@@ -256,38 +265,51 @@ def get_next_round_fee(options: dict, current_fee: float) -> float:
     elif options.working_fee_mode == "exponential":
         return current_fee + calc_perc(current_fee, options.working_fee_increase)
     else:
-        log.error("Working fee mode set incorrectly pass either 'linear' or 'exponetial'")
+        log.error("Working fee mode set incorrectly pass either 'linear' or 'exponential'")
         return current_fee
      
 def calc_perc(number: float, percentage: float):
     return number * (percentage / 100)
 
-# Runs every epoch based on thread_scheduler
-def run_every_epoch(options: dict):
-    
-    # Reference data
+def log_out_stats():
     global data
     statistics = data["statistics"]
     pending_messages = data["pending_messages"]
     working_messages = data["working_messages"]
 
+    pen = statistics["total_pending"]
+    wrk = statistics["total_worked"]
+    rep = statistics["total_replaced"]
+    max = statistics["max_fee"]
+    avg = statistics["avg_fee"]
+    age = statistics["avg_age"]
+    epc = statistics["current_epoch"]
+
+    # Log current runtime statistics
+    stats_message = (
+        "Stats:"
+        f" [Current Pending: {len(pending_messages)}] [Current Working: {len(working_messages)}]"
+        f" [Total Pending: {pen}] [Total Worked: {wrk}] [Total Replaced: {rep}]"
+        f" [Average Fee: {round(avg,9)}] [Max Fee: {round(max,9)}] [Average Age: {round(age)}] [Epoch: {epc}]"
+    )
+    log.info(stats_message)
+
+
+# Runs every epoch based on thread_scheduler
+def run_every_epoch(options: dict):
+    global data
+    statistics = data["statistics"]
+
     try:
         # Tick epoch
         statistics["current_epoch"] += 1
-
-        pen = statistics["total_pending"]
-        wrk = statistics["total_worked"]
-        rep = statistics["total_replaced"]
-        avg = statistics["avg_fee"]
-        max = statistics["max_fee"]
-        epc = statistics["current_epoch"]
-
-        # Log current runtime statistics
-        log.info(f"Running stats: [Current Pending: {len(pending_messages)}] [Current Working: {len(working_messages)}] [Total Pending: {pen}] [Total Worked: {wrk}] [Total Replacements: {rep}] [Average Fee: {round(avg,9)}] [Max Fee: {round(max,9)}] [Epoch: {epc}]")
-
+        
+        # Log out stats
+        log_out_stats()
+       
         # Export or save data each epoch
         if options.data_dir:
-            export_data(options)
+           export_data(options)
 
         # Run processing pipeline
         processing_pipeline(options)
@@ -300,12 +322,16 @@ def run_every_epoch(options: dict):
 
 # Thread scheduler to keep program running every epoch on the epoch start
 def thread_scheduler(options: dict) -> bool:
+    global data
     try:
 
-        # Check that the chain is in sync before scheduling a run could from time to time move timing out an epoch or two
+        # Check that the chain is in sync before scheduling a run accounting for tick in epoch
         while not check_sync_status():
-            log.warning(f"Chain out of sync waiting for 1 epoch or 30 seconds")
-            time.sleep(30)
+            now = datetime.now()
+            wait_seconds = 30 - (now.second % 30)
+            log.warning(f"Chain out of sync waiting till the top of next epoch to check again {wait_seconds} seconds.")
+            time.sleep(wait_seconds)
+            data["statistics"]["current_epoch"] += 1
 
         now = datetime.now()
         wait_seconds = 30 - (now.second % 30)
